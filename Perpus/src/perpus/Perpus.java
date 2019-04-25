651ba9a8031d2b0a55f2a18980d99cdd9078bd0f
Rauf Mazzidan <abd_mazzidan_25rpl@student.smktelkom-mlg.sch.id>
2019-04-25 18:50:11 +0700

    Add Project

---
+++ Perpus/src/perpus/Perpus.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perpus;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+/**
+ *
+ * @author Rauf Mazzidan
+ */
+public class Perpus {
+
+    final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    final String DB_URL = "jdbc:mysql://localhost/perpustakaan";
+    final String USER = "root";
+    final String PASS = "kopi";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
